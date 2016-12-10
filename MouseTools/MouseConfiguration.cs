0bdaac641f1ae3f4311a4bb3c108cbc4044ee30d
gabyzaaf <zafrani.prog@gmail.com>
2016-12-10 09:49:56 +0100

    modify the GetArray function and add the exception

--- MouseTools/MouseConfiguration.cs
+++ MouseTools/MouseConfiguration.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 namespace MouseTools
 {
@@ -14,16 +11,17 @@ namespace MouseTools
         {
             if (!dicos.ContainsKey("log"))
             {
-                return null;
+                throw new Exception("the dictionnary don't contains the key log ");
             }
             if (String.IsNullOrEmpty(dicos["log"]))
             {
-                return null;
-            }else
+                throw new Exception("The log values not exist ");
+            }
+            else
             {
                 string log = dicos["log"];
                 if (!Directory.Exists(log)) {
-                    return null;
+                    throw new Exception("the directory not exist ");
                 }
                 else
                 {
@@ -36,18 +34,18 @@ namespace MouseTools
         {
             if (!dicos.ContainsKey("file"))
             {
-                return null;
+                throw new Exception("the dictionnary don't contains the key file ");
             }
             if (String.IsNullOrEmpty(dicos["file"]))
             {
-                return null;
+                throw new Exception("the value from the key file in the dictionnary is null or empty  ");
             }
             string file = dicos["file"];
             string[] lines = File.ReadAllLines(file);
             int size = lines.Count();
             if (size==0)
             {
-                return null;
+                throw new Exception("the file don't contain value inside");
             }
             int sizeLine = 0;
             foreach (string value in lines)
@@ -55,7 +53,7 @@ namespace MouseTools
                 sizeLine = value.Count();
                 if (value[0]!=star || value[sizeLine-1]!= star)
                 {
-                    return null;
+                    throw new Exception("The wall is not identic ");
                 }
             }
             return lines;
@@ -64,26 +62,19 @@ namespace MouseTools
         public override Node[,] GetNodeArray()
         {
             Node[,] nodes = null;
-            try
+            string[] array = GetArray();
+            int height = array.GetLength(0);
+            nodes = new Node[height, array[0].Count()];
+            int width = nodes.GetLength(1);
+            int i = 0;
+            foreach (string line in array)
             {
-                string[] array = GetArray();
-                int height = array.GetLength(0);
-               nodes = new Node[height, array[0].Count()];
-                int width = nodes.GetLength(1);
-                for (int i = 0; i < height; i++)
+                for (int j=0;j<line.Count();j++ )
                 {
-                    for (int j = 0; j < width-1; j++)
-                    {
-                        nodes[i, j] = new Node(i, j,array[i][j]);
-                    }
+                    nodes[i, j] = new Node(i,j,line[j]);
                 }
+                i++;
             }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            
-
             return nodes;
         }
     }
